fbe6caaa40d67f4541117fff31cee5a391736a87
dharani2005 <58557415+dharani2005@users.noreply.github.com>
2019-12-06 19:54:52 +0100

    added new m3 method in A class

--- practice project/src/com/as/cls/A.java
+++ practice project/src/com/as/cls/A.java
@@ -15,10 +15,17 @@ public static void m1()
 public void m2()
 {
 	m1();
+
+
 	
 	System.out.println("this is m2 instance method in A class cls pakage " + z);
 	}
-	 
+   public  void m3()
+   {
+   System.out.println("this is m3 method");
+   }
+   
+   
 	public static void main(String[] args) {
 		
      A obj = new A();
